2d5601a0a633e4deed2623f1aeb6ac75454196c4
versatile153 <ceze46321@mail.com>
2022-07-05 13:14:14 +0100

    first

---
+++ index.js
@@ -0,0 +1,99 @@
+
+// // // single line commentl
+
+// // /*
+
+// // multi line comment
+
+
+
+
+// // */
+// // // there are three ways to declare a variable
+// // //var
+// // //let
+// // //const
+// // var name='versatile'
+// // var name="celestine"
+// // name='education'
+// // console.log(name)
+
+// // let firstName='ayogu'
+// // firstName='mathematics'
+
+// // console.log(firstName)
+// // // const string='everatis'
+// // const string='versatile'
+// // // string='university'
+// // console.log(string)
+
+// // //rules for variables declaration 
+// // //must be meaningful
+// // // can start with letter, underscore or dollar sign
+// // // must not be a reserved key world
+// // //should follow a particular convention
+// // //camel case
+// // const myFirstMajorName='versatile'
+// // //pascal case
+
+
+// // //data types
+
+// // // 1. primmitive data types
+// // //2. reference data types
+
+// // //primmitive
+// // //1/ string; sequence of characters
+// // //2. number
+// // //3 boolean
+// // //4 undefined
+// // //null
+// // //symbols
+
+// // //refer------object, array ,a type of object
+// // ``  //backticks
+// // ''  // single quote
+// // ""  // double quote
+
+// let string='44'
+// let score1 =344
+// const isTrue=true
+
+// let score2='17'
+// console.log(score1/score2)
+// console.log(score1/string)
+
+
+// console.log(400>78)
+
+//  if(score1>35){
+//      console.log('it is true')
+
+//  }else{
+// console.log('it is not true')
+//  }
+
+// console.log(typeof string)
+// console.log(typeof score1)
+// console.log(typeof isTrue)
+// console.log(string/score2)
+//  let x  
+ 
+//  let y=null
+//  console.log(typeof y)
+
+let score1=38
+let string='hello'
+console.log(score1/string)
+
+document.write(typeof NaN)
+document.querySelector("h1").innerHTML='welcome to our company'
+
+
+
+
+
+
+
+
+
