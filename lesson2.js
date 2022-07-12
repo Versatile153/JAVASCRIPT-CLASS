16e17a4d7eca2e3ada2cce1adb9a5bd0e0460330
versatile153 <ceze46321@mail.com>
2022-07-12 09:20:50 +0100

    third

--- lesson2.js
+++ lesson2.js
@@ -64,6 +64,9 @@ document.addEventListener("DOMContentLoaded",function(){
 document.querySelectorAll('button').forEach(function(button){
     button.onclick=function(){
         document.querySelector("h1").style.color=button.dataset.color
+        document.querySelector('h2').textContent=Date()
+       
+        
     }
 
 })
